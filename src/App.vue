215744c68a3845c1fcc8c10dd7558f10869e82e8
Tom Murphree <tmurph1810@gmail.com>
2018-09-18 16:23:23 -0500

    style top nav links

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,6 @@
         <router-link class="nav-link" to="/about" data-cy-about>About</router-link>
         <router-link class="nav-link" to="/help" data-cy-help>Help</router-link>
       </nav>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
     <router-view/>
   </div>
 </template>
@@ -29,5 +25,8 @@
 </script>
 
 <style lang="scss">
+  .nav-link {
+    color: #fff;
+  }
 
 </style>
