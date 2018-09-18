094b7cabb52149535210583ecf92df3bdd5fd66a
Tom Murphree <tmurph1810@gmail.com>
2018-09-17 22:10:52 -0500

    style top bar: move links to the left

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <!-- layout partially borrowed from https://getbootstrap.com/docs/4.1/examples/dashboard/  -->
   <div id="app">
-      <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <span class="navbar-brand col-sm-3 col-md-2 mr-0">Portfolio Dashboard</span>
+      <nav class="navbar navbar-dark bg-dark 
+        flex-md-nowrap justify-content-start 
+        p-0 shadow"
+      >
+        <div class="navbar-brand col-sm-3">Portfolio Dashboard</div>
         <router-link class="nav-link" to="/save" data-cy-save>Save</router-link>
         <router-link class="nav-link" to="/about" data-cy-about>About</router-link>
         <router-link class="nav-link" to="/help" data-cy-help>Help</router-link>
