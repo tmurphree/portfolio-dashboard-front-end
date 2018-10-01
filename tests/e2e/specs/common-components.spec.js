34c0e8167eae04f349f6f72e6fc83aef5886cc23
Tom Murphree <tmurph1810@gmail.com>
2018-09-30 22:58:52 -0500

    make side nav

--- tests/e2e/specs/common-components.spec.js
+++ tests/e2e/specs/common-components.spec.js
@@ -25,15 +25,30 @@ describe('side bar', function() {
     cy.visit('/');
   });
 
-  it('has a sidebar that goes all the way down', function() {
+  it('has appropriate styling', function() {
     cy.get('[data-cy-sidebar]')
       .should('have.css', 'overflow', 'hidden')
       .should('have.css', 'max-width', '25%')
       .its('height').should('be.gte', '600px');
-      
+  });
+
+  it('has these menus', function() {
+    cy.get('[data-cy-sidebar] li.nav-item')
+      .its('length').should('eq', 5);
+
+    cy.get('[data-cy-sidebar] li.nav-item a')
+      .should(($link) => {
+        expect($link[0]).to.have.class('active');     
+        expect($link[0].innerText).to.contain('Upload or edit current assets');
+
+        expect($link[1].innerText).to.contain('Graph view');
+        expect($link[2].innerText).to.contain('Table view');
+        expect($link[3].innerText).to.contain('Monitored securities');
+
+        expect($link[4].innerText).to.contain('Trade simulator');
+      });
   });
   
-
-
+  
   
 });
