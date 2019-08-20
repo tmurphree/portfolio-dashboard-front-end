842baa95433972794e65f5c67935ae605bcd8f80
Tom Murphree <tmurph1810@gmail.com>
2019-08-20 12:46:19 -0500

    consolidate home view tests into one file

--- tests/e2e/specs/home.spec.js
+++ tests/e2e/specs/home.spec.js
@@ -12,12 +12,7 @@ describe('the home view', () => {
       .should('not.have.attr', 'required');
   });
 
-  // skip until you get the API ready
-  it.skip('gets an array of objects from the API after file upload', () => {
-
-  });
-
-  function enterSecurityData() {
+  const enterSecurityData = function enterSecurityData() {
     cy.get('#symbol')
       .type('vtibx')
       .get('#friendly-name')
@@ -27,7 +22,7 @@ describe('the home view', () => {
       .get('#pct-bond-international')
       .type(100)
       .type('{enter}');
-  }
+  };
 
   it('lets you enter securities in one at a time', () => {
     cy.get('[cy-portfolio-table] tr')
@@ -55,7 +50,19 @@ describe('the home view', () => {
       });
   });
 
+  it('lets you clear the portfolio', () => {
+    cy.contains('Clear current portfolio').click();
+
+    cy.get('[cy-portfolio-table] tr')
+      .should('not.exist');
+  });
+
   it.skip('lets you edit the securities in your portfolio', () => {
+
+  });
+
+  // skip until you get the API ready
+  it.skip('gets an array of objects from the API after file upload', () => {
 
   });
 });
