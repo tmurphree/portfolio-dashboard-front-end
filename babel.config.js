6b05ae6385b2dd2f3961c9380b0423535e690874
Tom Murphree <tmurph1810@gmail.com>
2019-08-28 12:54:36 -0500

    fix Cypress issue 2945

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+// Use unusual structure so we can run Cypress tests with .should(callback).
+// See https://github.com/cypress-io/cypress/issues/2945.
+
+const regularSetup = {
   presets: [
     '@vue/app',
   ],
 };
+
+module.exports = process.env.CYPRESS_ENV ?
+  {} :
+  regularSetup;
