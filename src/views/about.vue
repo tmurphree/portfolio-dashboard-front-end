7237399633104f7030c094a0a8e05a5b0a97bcac
Tom Murphree <tmurph1810@gmail.com>
2019-09-10 00:40:01 -0500

    add messaging about how and why values are rounded

--- src/views/about.vue
+++ src/views/about.vue
@@ -7,12 +7,24 @@
       development skills in a way that's interesting to me.
     </p>
     <p>
-      This site attempts to be accurate, but <strong>I cannot guarantee that it is accurate</strong>.
+      This site attempts to be accurate, but I cannot guarantee that it is accurate.
     </p>
     <p>
       <strong>
         Do not make financial decisions based on this site.
       </strong>
+    </p>
+    <h2>How numbers are rounded on this site</h2>
+    <p>
+      Because the objective of the site is to provide a big-picture overview of your asset allocation,
+      certain numbers are rounded so that you don't get lost in the minutae.
+    </p>
+    <p class="d-none d-md-block">
+      The "Value" column is rounded to the nearest dollar.
+    </p>
+    <p>
+      The "% of Portfolio" column and graph are rounded to the nearest 1/10 of 1%.  As such, percentages may
+      add to slightly higher or lower than 100%.
     </p>
   </div>
 </template>
--- src/views/current-graph.vue
+++ src/views/current-graph.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>Current graph</h1>
+    A word on <router-link to="/about">rounding</router-link>.
   </div>
 </template>
