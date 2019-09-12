420eedb4d6ff06d5716d0e20105197730c195559
Tom Murphree <tmurph1810@gmail.com>
2019-09-12 13:12:56 -0500

    change graph view layout to row / column structure

--- src/views/graph.vue
+++ src/views/graph.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
-    <h1>Graph</h1>
-    <small class="col-12 pl-0">A word on <router-link to="/about">rounding</router-link>.</small>
-    <p>Coming soon.</p>
+    <div class="row">
+      <div class="col-12">
+        <h1>Graph</h1>
+        <small class="col-12 pl-0">
+          Prices are update once a minute.  A word on <router-link to="/about">rounding</router-link>.
+        </small>
+      </div>
+    </div>
+    <div class="row"> 
+      <div class="col-12">
+        <canvas id="current-portfolio"></canvas>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
