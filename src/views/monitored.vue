4834070bf62c6b145dbfe74cc42126be6cbe5770
Tom Murphree <tmurph1810@gmail.com>
2019-09-24 16:50:13 -0500

    draw a bar chart with nice axes

--- src/views/monitored.vue
+++ src/views/monitored.vue
@@ -11,7 +11,7 @@
     </section>
     <section class="col-12" id="graph-section">
       <canvas
-        aria-label="Monitored security chard"
+        aria-label="Monitored security chart"
         id="monitored-securities"
         role="img"
       >
@@ -21,9 +21,77 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   data: function data() {
     return {};
+  },
+  computed: {
+    monitoredSecurities() {
+      return this.portfolio
+        .filter((el) => el.monitored)
+        .map((el) => { 
+          return {
+            lowerBound: el.monitoredLowerBound,
+            upperBound: el.monitoredUpperBound,
+            symbol: el.symbol,
+          }
+        });
+    },
+    ...mapState(['portfolio']),
+  },
+  methods: {
+    /**
+     * @description Draw a chart.
+     * @param {string} selector Query selector specific to the <canvas> you want to draw
+     *    the chart on.
+     * @param {ChartInfo[]} chartInfo Data to chart.
+     * @returns {undefined}
+     * 
+     * @typedef ChartInfo
+     * @prop {string} backgroundColor 'rgba(255, 99, 132, 1)'
+     * @prop {number} data The data you want to show in the chart.
+     * @prop {string} label The label for the data point.
+    */
+    drawBarChart(selector='canvas', chartInfo) {
+      const context = document.querySelector(selector).getContext('2d');
+      console.log(context);
+      const data = {
+        labels: ['test bar chart'],
+        datasets: [{
+          backgroundColor: 'rgba(255, 99, 132, 1)',
+          data: [12],
+          borderWidth: 1
+        }]
+      };
+
+      return new Chart(
+        context,
+        {
+          type: 'bar',
+          data,
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  stepSize: 1,
+                  min: 0,
+                  // TODO: set suggestedMax to monitoredUpperBound
+                  suggestedMax: 9,
+                }
+              }],
+            },
+            legend: {
+              display: false
+            },
+          },
+        }
+      );
+    }
+  },
+  mounted() {
+    this.drawBarChart();
   },
   name: 'monitored'
 }
